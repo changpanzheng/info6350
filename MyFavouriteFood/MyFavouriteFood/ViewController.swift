93bc2f60d91bd619bdf930dff0c2d5ad35941f72
CHANGPAN ZHENG <changpanzheng@Parkers-MacBook-Pro.local>
2022-09-23 23:33:11 -0700

    basic myFavouriteFood initialize

---
+++ MyFavouriteFood/MyFavouriteFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavouriteFood
+//
+//  Created by CHANGPAN ZHENG on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
